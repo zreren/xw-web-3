b4e8f617ce0effba42fc87faaec7c66ba5b68db1
yunfan <1936341390@qq.com>
2023-02-13 20:34:49 +0800

    feat: 调整样式

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -37,7 +37,7 @@ const Navbar: React.FC<NavBarProps> = (props) => {
       </div>
       <Link href='/contact' className='md:text-3xl text-2xl ml-10 font-bold'>Infos</Link>
     </div>
-    <div onClick={toogleNavigation}>
+    <div className="cursor-pointer" onClick={toogleNavigation}>
       <Image src={navigationlogo} alt="navlogo" />
     </div>
     </nav>
