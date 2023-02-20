d7994a68dcf3cc70afdce0b028efb2ceba6479fe
yunfan <1936341390@qq.com>
2023-02-20 17:59:31 +0800

    feat: 越餐界面开发

--- src/pages/projectMenu/vietnamien/index.tsx
+++ src/pages/projectMenu/vietnamien/index.tsx
@@ -1,8 +1,39 @@
 import React from "react";
+import styles from './index.module.css';
+import cs from 'classnames';
+import vietnamien1 from '@/public/images/foods/vietnamien/vietnamien1.png'
+import vietnamien2 from '@/public/images/foods/vietnamien/vietnamien2.png'
+import edesignLogo from '@/public/images/edesignLogo.png';
+import FooterLocation from "@/components/FooterLocation";
 
 export default function Vietnamien() {
 
   return (
-    <div>Vietnamien</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <img src={edesignLogo.src} alt="" />
+      </div>
+      <div className={styles.title}>
+        <span>Vietnamien</span>
+        <div>越餐</div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.contentItem}>
+          <img src={vietnamien1.src} alt="" />
+        </div>
+        <div className={cs(
+          'flex items-center',
+          styles.contentItem
+        )}>
+        <img src={vietnamien2.src} alt="" />
+        </div>
+        <div className="opacity-0">
+        {/* <img src={vietnamien2.src} alt="" /> */}
+        </div>
+      </div>
+        <div className="fixed bottom-0 md:px-12 px-2 w-full">
+        <FooterLocation leftLocation="/projectMenu?active=design" />
+        </div>
+    </div>
   )
 }
