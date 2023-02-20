85be2d248b08f068d290647f97a637af13e1a9c2
yunfan <1936341390@qq.com>
2023-02-20 20:16:48 +0800

    feat: 酒吧韩餐界面

--- src/pages/projectMenu/bar/index.tsx
+++ src/pages/projectMenu/bar/index.tsx
@@ -1,10 +1,36 @@
 import React from "react";
+import styles from './index.module.css';
+import bar1 from '@/public/images/bar/Bar-1.png'
+import bar2 from '@/public/images/bar/Bar-2.png'
+import bar3 from '@/public/images/bar/Bar-3.png'
+import edesignLogo from '@/public/images/edesignLogo.png';
+import FooterLocation from "@/components/FooterLocation";
 
-export default function Bar() {
+export default function Coreen() {
 
   return (
-    <div>
-      Bar
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <img src={edesignLogo.src} alt="" />
+      </div>
+      <div className={styles.title}>
+        <span>Bar</span>
+        <div>酒吧</div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.contentItem}>
+          <img src={bar1.src} alt="" />
+        </div>
+        <div className={styles.contentItem}>
+        <img src={bar2.src} alt="" />
+        </div>
+        <div className={styles.contentItem}>
+        <img src={bar3.src} alt="" />
+        </div>
+      </div>
+        <div className="fixed bottom-0 md:px-12 px-2 w-full">
+        <FooterLocation leftLocation="/projectMenu?active=design" />
+        </div>
     </div>
   )
 }
--- src/pages/projectMenu/coreen/index.tsx
+++ src/pages/projectMenu/coreen/index.tsx
@@ -1,8 +1,35 @@
 import React from "react";
+import styles from './index.module.css';
+import coreen1 from '@/public/images/foods/coreen/coreen1.png'
+import coreen2 from '@/public/images/foods/coreen/coreen2.png'
+import edesignLogo from '@/public/images/edesignLogo.png';
+import FooterLocation from "@/components/FooterLocation";
 
 export default function Coreen() {
 
   return (
-    <div>Coreen</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <img src={edesignLogo.src} alt="" />
+      </div>
+      <div className={styles.title}>
+        <span>Coréen</span>
+        <div>韩餐</div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.contentItem}>
+          <img src={coreen1.src} alt="" />
+        </div>
+        <div className={styles.contentItem}>
+        <img src={coreen2.src} alt="" />
+        </div>
+        <div className="opacity-0">
+        <img src={coreen1.src} alt="" />
+        </div>
+      </div>
+        <div className="fixed bottom-0 md:px-12 px-2 w-full">
+        <FooterLocation leftLocation="/projectMenu?active=design" />
+        </div>
+    </div>
   )
 }
