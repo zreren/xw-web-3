d40f9682f69600173f764e44a1040c6f523bcfe6
yunfan <1936341390@qq.com>
2023-02-20 16:39:18 +0800

    feat: 禁止屏幕右击事件

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
         __html: `
           (function(){
             if(window && document) {
-              console.log('niming');
               document.oncontextmenu=new Function("event.returnValue=false");
               document.onselectstart=new Function("event.returnValue=false");
             }
